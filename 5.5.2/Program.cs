2045944ea129f0e601081645ab107b454c1364e9
Олег Воронцов <oleg-voroncov@mail.ru>
2022-12-20 18:06:02 +0300

    Добавьте файлы проекта.

---
+++ 5.5.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _5._5._2
+{
+    class Program
+    {
+        //метод смены порядка слов в предложении на противоположный
+        static string ReversWords(string inputPhrase)
+        {
+            string[] Words = Reverse(inputPhrase);
+
+            string outputPhrase = "";
+
+            for (int i = Words.GetLength(0) - 1; i >= 0; i--)
+            {
+                outputPhrase += Words[i] + " ";
+            }
+            return outputPhrase;
+        }
+
+        //метод разделения предложенния на слова
+        static string[] Reverse(string text)
+        {
+            string[] Words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return Words;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите предложение, каждое слово отделите пробелом: ");
+            string inputPhrase = Console.ReadLine();
+
+            string reversePhrase = ReversWords(inputPhrase);
+            Console.Write("Ваше предложение наоборот: {0}", reversePhrase);
+
+            Thread.Sleep(7000);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5_HomeWork
+{
+    class Program
+    {
+        static string[] SplitText(string EnterTextLine)
+        {
+            string[] Words = EnterTextLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return Words;
+        }
+
+        static void PrintWords(string[] EnterWords)
+        {
+            foreach (string Word in EnterWords)
+            {
+                Console.WriteLine(Word);
+            }
+        }
+
+        //программа разделяет предложение на слова и выводит слова с новой строки
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите предложение, каждое слово отделите пробелом: ");
+            string TextLine = Console.ReadLine();
+
+            string[] Words = SplitText(TextLine);
+
+            PrintWords(Words);
+
+            Console.ReadLine();
+        }
+    }
+}
